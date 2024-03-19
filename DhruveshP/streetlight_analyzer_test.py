0ea1efd3721e4d7977a202d7e0743d0bc08c1185
Dhruvesh Panchal <panc0075@algonquinlive.com>
2024-03-19 10:22:10 -0400

    Changes to be committed:

--- DhruveshP/streetlight_analyzer_test.py
+++ DhruveshP/streetlight_analyzer_test.py
@@ -9,4 +9,16 @@ def test_get_streetlight_count():
     actual_count = streetlight_analyzer.get_streetlight_count("CARLING", 0.0002)
     assert expected_count == actual_count
 
+def test_get_unique_values():
+    expected_values = {"Road1", "Road2", "Road3"}  
+    actual_values = streetlight_analyzer._get_unique_values("E:\acgis\gis4207_prog\Data\Ottawa\Road_Centrelines", "FIELD_NAME")  
+    assert expected_values == actual_values
+
+def test_save_streetlights():
+    streetlight_analyzer.save_streetlights("CARLING", 0.0002, "output_streetlights_fc")
+
+def test_show_road_names():
+    streetlight_analyzer.show_road_names()
+  
+
 
